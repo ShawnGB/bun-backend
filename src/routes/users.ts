154141bcbec11d07dd4a81cace3252e1f3102bd3
Shawn GB <god.shawn@gmail.com>
2023-09-12 15:47:20 +0200

    basic user routes and hadnlers

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,19 @@
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
+import { getAllUsers, createUser, getUser } from '../controllers/user';
 
 const users = new Elysia().group('users', (app) =>
   app
-    .get('/', () => 'user')
-    .get('/:id', () => 'user')
-    .post('/', () => '')
+
+    .get('/', () => getAllUsers())
+    .get('/:id', ({ params: { id } }) => getUser(id))
+    .post('/', ({ body }) => createUser(body), {
+      body: t.Object({
+        firstName: t.String(),
+        lastName: t.String(),
+        email: t.String(),
+        password: t.String(),
+      }),
+    })
     .patch('/:id', () => '')
     .delete('/:id', () => '')
 );
