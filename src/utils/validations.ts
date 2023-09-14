c328a6f12f7522c96b3ef237e525fd38732b5867
Shawn GB <god.shawn@gmail.com>
2023-09-14 17:17:59 +0200

    added UUID validation

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,9 +1,12 @@
 import { prisma } from '../lib/prisma';
 import { errorResponse } from './responses';
 
+const EMAIL_REGEX = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+const UUID_REGEX =
+  /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
+
 export const isValidEmail = (email: string): boolean => {
-  const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-  return regex.test(email);
+  return EMAIL_REGEX.test(email);
 };
 
 export const doesUserExist = async (
@@ -16,18 +19,25 @@ export const doesUserExist = async (
 };
 
 export const checkId = (id: string) => {
-  return !id ? errorResponse('Missing id.') : null;
+  if (!id) return errorResponse('Missing id.');
+  if (!UUID_REGEX.test(id)) return errorResponse('Invalid id.');
+  return null;
 };
 
 export const checkUserBody = async (body: UserUpdateBody | UserCreateBody) => {
   if (body.email) {
-    if (!isValidEmail(body.email))
+    if (!isValidEmail(body.email)) {
       return errorResponse('Invalid email address.');
-    if (await doesUserExist('email', body.email))
+    }
+
+    if (await doesUserExist('email', body.email)) {
       return errorResponse('Email address already in use.');
+    }
   }
+
   if (body.userName && (await doesUserExist('userName', body.userName))) {
     return errorResponse('User name already in use.');
   }
+
   return null;
 };
