5f2cf2a0ca511510f96429bcdf93fd8e503aa228
Shawn GB <god.shawn@gmail.com>
2023-09-14 13:58:55 +0200

    clean up controller code, move type to types dir

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,30 +1,36 @@
 import { prisma } from '../lib/prisma';
-import { isValidEmail } from '../utils/validations';
+import { isValidEmail, doesUserExist } from '../utils/validations';
 import { errorResponse } from '../utils/responses';
-import { doesUserExist } from '../utils/validations';
 
-interface UserRequestBody {
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-  userName: string;
-  summary?: string;
-  profileImage?: string;
-}
+const getAllUsers = async () => prisma.user.findMany();
 
-//  get all users
-const getAllUsers = async () => await prisma.user.findMany();
+const getUser = async (id: string) => {
+  if (!id) {
+    return errorResponse('Missing id.');
+  }
+  return prisma.user.findUnique({ where: { id } });
+};
 
-// get a user by id
-const getUser = async (id: string) =>
-  id ? await prisma.user.findUnique({ where: { id } }) : null;
+const updateUser = async (id: string, body: UserRequestBody) => {
+  if (!id) {
+    return errorResponse('Missing id.');
+  }
+  return prisma.user.update({
+    where: { id },
+    data: { ...body },
+  });
+};
 
-// create an new user
+const deleteUser = async (id: string) => {
+  if (!id) {
+    return errorResponse('Missing id.');
+  }
+  return prisma.user.delete({ where: { id } });
+};
+
 const createUser = async (userBody: UserRequestBody) => {
   const { email, userName, password } = userBody;
 
-  // validations before creating a user
   if (!isValidEmail(email)) {
     return errorResponse('Invalid email address.');
   }
@@ -37,7 +43,6 @@ const createUser = async (userBody: UserRequestBody) => {
     return errorResponse('User name already in use.');
   }
 
-  //create a user
   const hash = await Bun.password.hash(password);
   const user = await prisma.user.create({
     data: { ...userBody, password: hash },
@@ -51,21 +56,5 @@ const createUser = async (userBody: UserRequestBody) => {
       }
     : errorResponse('User could not be created.');
 };
-
-// update a user
-const updateUser = async (id: string, body: UserRequestBody) => {
-  id
-    ? await prisma.user.update({
-        where: { id },
-        data: { ...body },
-      })
-    : errorResponse('Missing id.');
-};
-
-// delete a user
-const deleteUser = async (id: string) =>
-  id
-    ? await prisma.user.delete({ where: { id } })
-    : errorResponse('Missing id.');
 
 export { getAllUsers, getUser, createUser, updateUser, deleteUser };
---
+++ src/tyoes/user.d.ts
@@ -0,0 +1,9 @@
+interface UserRequestBody {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  userName: string;
+  summary?: string;
+  profileImage?: string;
+}
