286458bcf85a13a312d8ef87298f3a8486edf8b7
Shawn GB <god.shawn@gmail.com>
2023-09-14 14:57:00 +0200

    moved validation to extra file

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,49 +1,14 @@
 import { prisma } from '../lib/prisma';
-import { isValidEmail, doesUserExist } from '../utils/validations';
 import { errorResponse } from '../utils/responses';
+import { checkId, checkUserBody } from '../utils/validations';
 
-const getAllUsers = async () => prisma.user.findMany();
+const getAllUsers = () => prisma.user.findMany();
 
-const getUser = async (id: string) => {
-  if (!id) {
-    return errorResponse('Missing id.');
-  }
-  return prisma.user.findUnique({ where: { id } });
-};
+const createUser = async (userBody: UserCreateBody) => {
+  const bodyError = await checkUserBody(userBody);
+  if (bodyError) return bodyError;
 
-const updateUser = async (id: string, body: UserRequestBody) => {
-  if (!id) {
-    return errorResponse('Missing id.');
-  }
-  return prisma.user.update({
-    where: { id },
-    data: { ...body },
-  });
-};
-
-const deleteUser = async (id: string) => {
-  if (!id) {
-    return errorResponse('Missing id.');
-  }
-  return prisma.user.delete({ where: { id } });
-};
-
-const createUser = async (userBody: UserRequestBody) => {
-  const { email, userName, password } = userBody;
-
-  if (!isValidEmail(email)) {
-    return errorResponse('Invalid email address.');
-  }
-
-  if (await doesUserExist('email', email)) {
-    return errorResponse('Email address already in use.');
-  }
-
-  if (await doesUserExist('userName', userName)) {
-    return errorResponse('User name already in use.');
-  }
-
-  const hash = await Bun.password.hash(password);
+  const hash = await Bun.password.hash(userBody.password);
   const user = await prisma.user.create({
     data: { ...userBody, password: hash },
   });
@@ -57,4 +22,16 @@ const createUser = async (userBody: UserRequestBody) => {
     : errorResponse('User could not be created.');
 };
 
-export { getAllUsers, getUser, createUser, updateUser, deleteUser };
+const getUser = (id: string) =>
+  checkId(id) || prisma.user.findUnique({ where: { id } });
+
+const updateUser = async (id: string, body: UserUpdateBody) => {
+  const error = checkId(id) || (await checkUserBody(body));
+  if (error) return error;
+  return prisma.user.update({ where: { id }, data: { ...body } });
+};
+
+const deleteUser = (id: string) =>
+  checkId(id) || prisma.user.delete({ where: { id } });
+
+export { createUser, getAllUsers, getUser, updateUser, deleteUser };
--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,8 +1,7 @@
-// function to check if an email has a valifd format, and if it does, it returns true, otherwise it returns false.
-
 import { prisma } from '../lib/prisma';
+import { errorResponse } from './responses';
 
-export const isValidEmail = (email: string) => {
+export const isValidEmail = (email: string): boolean => {
   const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   return regex.test(email);
 };
@@ -11,10 +10,24 @@ export const doesUserExist = async (
   key: 'email' | 'userName',
   value: string
 ) => {
-  const whereCondition: any = {};
-  whereCondition[key] = value;
-
-  return await prisma.user.findUnique({
-    where: whereCondition,
+  return prisma.user.findUnique({
+    where: { [key]: value } as any,
   });
+};
+
+export const checkId = (id: string) => {
+  return !id ? errorResponse('Missing id.') : null;
+};
+
+export const checkUserBody = async (body: UserUpdateBody | UserCreateBody) => {
+  if (body.email) {
+    if (!isValidEmail(body.email))
+      return errorResponse('Invalid email address.');
+    if (await doesUserExist('email', body.email))
+      return errorResponse('Email address already in use.');
+  }
+  if (body.userName && (await doesUserExist('userName', body.userName))) {
+    return errorResponse('User name already in use.');
+  }
+  return null;
 };
